865df2313b520cd0db27794ddce57228d6fdfe86
Yashaswini Rajanna <74413190+YashaswiniRajanna@users.noreply.github.com>
2022-04-10 21:12:06 +0530

    Time: 4 ms (71.34%), Space: 8.4 MB (28.60%) - LeetHub

---
+++ 682-baseball-game/682-baseball-game.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int calPoints(vector<string>& ops) {
+        int n=ops.size();
+        vector<int>res;
+        int ans=0;
+        for(string i:ops){
+            if(i=="+")res.push_back(res[res.size()-2]+res[res.size()-1]);
+             else if(i=="D")   res.push_back(res[res.size()-1]*2);
+                else if(i=="C")res.pop_back();
+                          
+                  else 
+                        res.push_back(stoi(i));
+                    
+            }
+            return accumulate(res.begin(),res.end(),0);
+        }
+        
+};
