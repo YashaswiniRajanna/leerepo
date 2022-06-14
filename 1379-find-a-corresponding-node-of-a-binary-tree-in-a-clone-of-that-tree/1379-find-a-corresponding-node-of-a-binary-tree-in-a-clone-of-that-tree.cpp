78f09759875fc7845ef603e33b3a1a7daae923a8
Yashaswini Rajanna <74413190+YashaswiniRajanna@users.noreply.github.com>
2022-06-14 07:50:22 +0530

    Time: 814 ms (42.43%), Space: 163.9 MB (57.69%) - LeetHub

--- 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
@@ -7,7 +7,7 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-
+/*MINE*/
 class Solution {
 public:
     TreeNode* ans=NULL;
@@ -23,7 +23,21 @@ public:
         return ans;
     }
 };
+//REFERENCE1
+// class Solution {
+// public:
+//  TreeNode* ans;
+//     TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
+//         if(cloned==NULL || original ==NULL ) return NULL;
+//         if(cloned->val==target->val)
+//             ans=cloned;
+//         getTargetCopy(original,cloned->left,target);
+//         getTargetCopy(original,cloned->right,target);
+//         return ans; 
+//     }
+// };
 
+//REFERENCE2
 // class Solution {
 // public:
     
