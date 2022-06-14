e24698ee78c639d65da124308576f71589113713
Yashaswini Rajanna <74413190+YashaswiniRajanna@users.noreply.github.com>
2022-06-14 07:48:57 +0530

    Time: 644 ms (73.79%), Space: 164 MB (20.17%) - LeetHub

--- 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
@@ -11,12 +11,40 @@
 class Solution {
 public:
     TreeNode* ans=NULL;
-    TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
-        if(cloned==NULL || original==NULL) return NULL;
-         getTargetCopy(original,cloned->left,target);
-        if(cloned->val==target->val)
+    void getans(TreeNode* cloned,TreeNode*  &target){
+        if(cloned==NULL) return;
+        if(cloned->val == target->val)
             ans=cloned;
-        getTargetCopy(original,cloned->right,target);
+        getans(cloned->left,target);
+        getans(cloned->right,target);
+    }
+    TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
+    getans(cloned,target);
         return ans;
     }
 };
+
+// class Solution {
+// public:
+    
+//     void inOrder(TreeNode* &cloned, int &keyValue, TreeNode* &reference)
+//     {
+//         if(cloned==NULL)
+//             return;
+        
+//         inOrder(cloned->left,keyValue,reference);
+//         if(cloned->val==keyValue)
+//         {
+//             reference=cloned;
+//             return;
+//         }
+//         inOrder(cloned->right,keyValue,reference);
+//     }
+    
+//     TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
+        
+//         TreeNode* reference=NULL;
+//         inOrder(cloned,target->val,reference);
+//         return reference;
+//     }
+// };
