9b17439790859b5ea7ca5d4bf5263aa049944812
Yashaswini Rajanna <74413190+YashaswiniRajanna@users.noreply.github.com>
2022-07-11 23:26:03 +0530

    Time: 287 ms (6.35%), Space: 93.3 MB (89.48%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,7 +1,24 @@
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         int i,j;
+//         int profit=0,maxprofit=0;
+//         //int min=prix[0];
+//         for(int i=0;i<prices.size()-1;i++){
+//             for(int j=i+1;j<prices.size();j++){
+//                 profit=prices[j]-prices[i];
+//                 if(profit>maxprofit){
+//                     maxprofit=profit;
+//                 }
+//             }
+//         }
+//         return maxprofit;
+//     }
+// };
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-       int minb=prices[0];
+        int minb=prices[0];
         int maxprice=0;
         for(int i=1;i<prices.size();i++){
             minb=min(minb,prices[i]);
