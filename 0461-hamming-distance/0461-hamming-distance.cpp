2cffad9bfa26f715ea37853a131fd63c5e633a1d
Yashaswini Rajanna <74413190+YashaswiniRajanna@users.noreply.github.com>
2023-12-26 00:25:44 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (18.05%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.cpp
+++ 0461-hamming-distance/0461-hamming-distance.cpp
@@ -1,7 +1,19 @@
 class Solution {
 public:
-    int hammingDistance(int x, int y) {
-        return __builtin_popcount(x ^ y);
+    int binaryto(int ans){
+        int count=0;
+        while(ans>0){
+        int n=ans%2;
+            count+=n;
+            cout<<ans;
+            ans/=2;
+        } 
+        return count;
         
+    }
+    int hammingDistance(int x, int y) {
+        
+        int ans= binaryto(x ^ y);
+        return ans;
     }
 };
