4f1ca0ca357574770b21293ccafa9ed0667ac821
Yashaswini Rajanna <74413190+YashaswiniRajanna@users.noreply.github.com>
2023-12-31 23:09:21 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (41.15%) - LeetHub

--- 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.cpp
+++ 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.cpp
@@ -2,40 +2,24 @@ class Solution {
 public:
     int maxLengthBetweenEqualCharacters(string s) {
         int count=0;
-        for(int left=0;left<s.size()-1;left++){
-            for(int right=left+1;right<s.size();right++){
-                if(s[left]==s[right]){
-                    count=max(count,right-left);
-                }
+        // for(int left=0;left<s.size()-1;left++){
+        //     for(int right=left+1;right<s.size();right++){
+        //         if(s[left]==s[right]){
+        //             count=max(count,right-left);
+        //         }
+        //     }
+        //     }
+        // return count-1;
+        
+        unordered_map<char,int>m;
+        for(int i=0;i<s.size();i++){
+           if(m.find(s[i])!=m.end()){
+               count=max(count, i-m[s[i]]);
+           }
+            else{
+                m[s[i]]=i;
             }
-            }
+        }
         return count-1;
-//         int start=0,end=s.size()-1;
-//         int mid=(start+end)/2;
-
-//         while(start<=s.size() && end>=0){
-//             if(s[start]==s[end]){
-//                 for(int i=start;i<end;i++){
-//                     count++;
-//                 }
-//             }
-//             start++;
-//             end--;
-//         }
-//       return count; 
-        
-//         unordered_map<char,int>m;
-//         int count=0;
-//         for(auto i : s){
-
-//             if(s[i]==0){
-//                 // count++;
-//                // s[i]=count;
-//             }
-//             else{
-//                 int end=i;
-//             }
-//         }
-        
     }
 };
