f4ea4cec603d5ee2de910c6f599871642199200e
Yashaswini Rajanna <74413190+YashaswiniRajanna@users.noreply.github.com>
2024-01-08 23:15:07 +0530

    Time: 114 ms (31.13%), Space: 61.1 MB (12.61%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -4,12 +4,32 @@ using namespace std;
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        sort(nums.begin(),nums.end());
-        // set<int>;
-        // int j=0;
-        //     for(int i=0;i<nums.szie();i++){
-        //         set[j++].insert(nums[i]);
-        //     }
-        return nums[nums.size()-k];
+        // sort(nums.begin(),nums.end());
+        // return nums[nums.size()-k];
+        priority_queue<int> p;
+        for(auto i:nums){
+            p.push(i);
+        }
+        while(k!=1){
+            p.pop();
+            k--;
+        }
+        return p.top();
     }
 };
+
+//using priority queue
+
+
+
+// if duplicate elements are not counted
+  // set<int> s;
+  //       int j=0;
+  //           for(int i=0;i<nums.size();i++){
+  //               s.insert(nums[i]);
+  //               // cout<<nums[i];
+  //           }
+  //       for(auto i :s){
+  //           cout<<i;
+  //       }
+  //       return nums[s.size()-1-k];
