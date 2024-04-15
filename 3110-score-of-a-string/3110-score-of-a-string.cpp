c6c93cfbc4c53681e136718e90efef5bb2cdc9c0
Yashaswini Rajanna <74413190+YashaswiniRajanna@users.noreply.github.com>
2024-04-15 23:56:12 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (40.00%) - LeetHub

---
+++ 3110-score-of-a-string/3110-score-of-a-string.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int res=0;
+        for(int i=0;i<s.size()-1;i++){
+            res=res+ (abs((int(s[i])-int(s[i+1]))));
+        }
+        return res;
+    }
+};
