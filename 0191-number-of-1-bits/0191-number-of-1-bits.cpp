df5fe9167e511c58bffdc97b94235a8e9640cf5e
Yashaswini Rajanna <74413190+YashaswiniRajanna@users.noreply.github.com>
2024-05-03 11:22:57 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (15.52%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        int ans=0;
-        while(n){
-            if(n&1){
-                ans++;
-                
-            }
-            n=n>>1;
+        int count=0;
+        while(n!=0){
+            n=n&(n-1);
+            count++;
         }
-        return ans;
-        
+        return count;
     }
 };
+
+
 
 
 
