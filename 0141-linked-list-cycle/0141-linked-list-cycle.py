fd7794aa8aa77c2c9d995cc598785b030c99a189
Yashaswini Rajanna <74413190+YashaswiniRajanna@users.noreply.github.com>
2024-05-04 23:33:29 +0530

    Time: 36 ms (95.18%), Space: 19.2 MB (32.96%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -1,24 +1,15 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
-class Solution {
-public:
-    bool hasCycle(ListNode *head) {
-        ListNode* slow=head;
-        ListNode* fast=head;
-        while(fast!=NULL && fast->next!=NULL){
-            slow=slow->next;
-            fast=fast->next->next;
-            if(fast==slow){
-                return true;
-            }
-        }
-        return false;
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow=fast=head;
+        while(fast and fast.next):
+            slow,fast=slow.next, fast.next.next
+            if(slow==fast):
+                return True
+        return False;
         
-    }
-};
