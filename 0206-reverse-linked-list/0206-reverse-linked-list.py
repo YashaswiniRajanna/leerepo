aa1bce2edf248afea355436751b90e3ef14c6763
Yashaswini Rajanna <74413190+YashaswiniRajanna@users.noreply.github.com>
2024-06-12 12:52:12 +0530

    Time: 36 ms (65.92%), Space: 17.6 MB (67.00%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -5,6 +5,9 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+#bruteforce- store ll elements in stack and pop and store back o ll => tc o(2n) scO(N)
+#optimal-point the ll node in reverse => tc O(n) sc O(1)
+
         prev=None
         cur=head
         next=None
