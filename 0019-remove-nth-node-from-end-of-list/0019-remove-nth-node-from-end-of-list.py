a74e82728fbebfcacda949396a0f0b4f58b1435f
Yashaswini Rajanna <74413190+YashaswiniRajanna@users.noreply.github.com>
2024-06-12 14:39:41 +0530

    Time: 43 ms (13.97%), Space: 16.4 MB (81.28%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -5,6 +5,9 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+#Bf= take a ll size-n and delete the node form front  TC-O(T + N) SC- O(1)
+#Optimal= fast at n and slow at n-1 and point slow next = slow next next TC-O(T + N) SC- O(1)
+
         temp=head
         fast=head
         slow=head
