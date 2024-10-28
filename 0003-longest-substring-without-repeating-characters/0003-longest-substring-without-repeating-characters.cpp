662fa18e61f4ccb62dcdddebaa6eceafedcfcfb1
Yashaswini Rajanna <74413190+YashaswiniRajanna@users.noreply.github.com>
2024-10-28 16:40:26 +0530

    Time: 11 ms (61.13%), Space: 11.9 MB (71.98%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,24 +1,47 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
+        // using map
+        
         int start=0,end=0;
-        unordered_set<char>se;
-        int n=s.size();
         int maxx=0;
+        int n=s.size();
+        unordered_map<char,int>mp;
         for(int end=0;end<n;end++){
-            if(se.count(s[end])==0){
-                se.insert(s[end]);
-                maxx=max(maxx,end-start +1);
+            if(mp.count(s[end])==0 ||mp[s[end]]<start){
+                mp[s[end]]=end;
+                maxx=max(maxx,end-start+1);
             }
-            else{
-                while(se.count(s[end])!=0){
-                    se.erase(s[start]);
-                    start++;
-                }
-                se.insert(s[end]);
-                // se[starr]=s[end];
-            }
+              else{
+                      // mp.clear();
+                      start=mp[s[end]]+1;
+                      mp[s[end]]=end;
+              }
         }
         return maxx;
-    }
-};
+       }
+   };
+               
+        // using set
+            
+        // int start=0,end=0;
+        // unordered_set<char>se;
+        // int n=s.size();
+        // int maxx=0;
+        // for(int end=0;end<n;end++){
+        //     if(se.count(s[end])==0){
+        //         se.insert(s[end]);
+        //         maxx=max(maxx,end-start +1);
+        //     }
+        //     else{
+        //         while(se.count(s[end])!=0){
+        //             se.erase(s[start]);
+        //             start++;
+        //         }
+        //         se.insert(s[end]);
+        //         // se[starr]=s[end];
+        //     }
+        // }
+        // return maxx;
+//     }
+// };
