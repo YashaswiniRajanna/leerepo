2b23b56c59d7e8480d7a536ebf3304be155942e6
Yashaswini Rajanna <74413190+YashaswiniRajanna@users.noreply.github.com>
2024-11-27 20:27:03 +0530

    Time: 0 ms, Memory: 20 MB - LeetHub

--- reverse-words-in-a-string-ii/reverse-words-in-a-string-ii.cpp
+++ reverse-words-in-a-string-ii/reverse-words-in-a-string-ii.cpp
@@ -1,40 +1,61 @@
 class Solution {
 public:
     void reverseWords(vector<char>& s) {
-        int original_size=s.size();
-        vector<char> word1;
-        vector<char> dup,dup1;
-        int i=0,j=s.size()-1;
-        while(i<s.size()){
-            if(s[i]!=' '){
-                word1.push_back(s[i]);
+        //2
+        reverse(s.begin(),s.end());
+
+        cout<<endl;
+        int start,end;
+        for(int i=0;i<s.size();i++){
+            start=i;
+            while(i<s.size() && isalnum(s[i])){
+                ++i;
             }
-            else if(s[i]==' '){
-                if(!word1.empty()){
-                    if(!dup.empty()){
-                        word1.push_back(' ');
-                        dup.insert(dup.begin(),word1.begin(),word1.end());
-                        word1.clear();
-                    }
-                    else{
-                        dup.insert(dup.begin(),word1.begin(),word1.end());
-                        word1.clear();
-                    }
-                }
-            }
-            i++;
+            if(i>s.size()) break;
+            end=i;
+            reverse(s.begin()+start,s.begin()+end);
         }
-        if(!word1.empty()){
-            if(!dup.empty()){
-                        word1.push_back(' ');
-                        dup.insert(dup.begin(),word1.begin(),word1.end());
-                        word1.clear();
-            }
-            else{
-                dup.insert(dup.begin(),word1.begin(),word1.end());
-                word1.clear();
-            }
-        }
-        s=dup;
+        // reverse(s.begin()+start,s.end());
+        
     }
 };
+        
+        // 1
+//         int original_size=s.size();
+//         vector<char> word1;
+//         vector<char> dup;
+//         int i=0,j=s.size()-1;
+//         while(i<s.size()){
+//             if(s[i]!=' '){
+//                 word1.push_back(s[i]);
+//             }
+//             else if(s[i]==' '){
+//                 if(!word1.empty()){
+//                     if(!dup.empty()){
+//                         word1.push_back(' ');
+//                         dup.insert(dup.begin(),word1.begin(),word1.end());
+//                         word1.clear();
+//                     }
+//                     else{
+//                         dup.insert(dup.begin(),word1.begin(),word1.end());
+//                         word1.clear();
+//                     }
+//                 }
+//             }
+//             i++;
+//         }
+//         if(!word1.empty()){
+//             if(!dup.empty()){
+//                         word1.push_back(' ');
+//                         dup.insert(dup.begin(),word1.begin(),word1.end());
+//                         word1.clear();
+//             }
+//             else{
+//                 dup.insert(dup.begin(),word1.begin(),word1.end());
+//                 word1.clear();
+//             }
+//         }
+//         s=dup;
+        
+//     }
+// };
