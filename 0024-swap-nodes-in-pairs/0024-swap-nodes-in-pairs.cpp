5779988e4d6dfe241d1eb59df503fc103c0c955d
Yashaswini Rajanna <74413190+YashaswiniRajanna@users.noreply.github.com>
2025-02-19 22:35:12 +0530

    Time: 0 ms (100%), Space: 11.2 MB (57.13%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
@@ -11,40 +11,59 @@
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
-        ListNode* newi=head;
-        vector<int> res;
-        while(newi!=NULL){
-            res.push_back(newi->val);
-            newi=newi->next;
-        }
-        for(int i=0;i<res.size();i++){
-            cout<<res[i]<<" ";
-        }
-        int i=0,j=1;
-        if(res.size()>1){
-            while(j<res.size()){
-            int temp=res[i];
-            res[i]=res[j];
-            res[j]=temp;
-              i+=2;
-              j+=2;
-        }
-        }
-      for(int i=0;i<res.size();i++){
-            cout<<res[i]<<" ";
-        }
-        if(res.size()>1){
-        ListNode* du=new ListNode(res[0]);
-        ListNode* duu=du;
-        int k=1;
-        while(k<res.size()){
-            duu->next = new ListNode(res[k++]);
-            duu=duu->next;
-        }
-        return du;
-        }
-        else{
-            return head;
-        }
+        //creating new vector
+    //     ListNode* newi=head;
+    //     vector<int> res;
+    //     while(newi!=NULL){
+    //         res.push_back(newi->val);
+    //         newi=newi->next;
+    //     }
+    //     for(int i=0;i<res.size();i++){
+    //         cout<<res[i]<<" ";
+    //     }
+    //     int i=0,j=1;
+    //     if(res.size()>1){
+    //         while(j<res.size()){
+    //         int temp=res[i];
+    //         res[i]=res[j];
+    //         res[j]=temp;
+    //           i+=2;
+    //           j+=2;
+    //     }
+    //     }
+    //   for(int i=0;i<res.size();i++){
+    //         cout<<res[i]<<" ";
+    //     }
+    //     if(res.size()>1){
+    //     ListNode* du=new ListNode(res[0]);
+    //     ListNode* duu=du;
+    //     int k=1;
+    //     while(k<res.size()){
+    //         duu->next = new ListNode(res[k++]);
+    //         duu=duu->next;
+    //     }
+    //     return du;
+    //     }
+    //     else{
+    //         return head;
+    //     }
+    // }
+
+    //without creating a vector and linkedlist
+    if(head==NULL || head->next==NULL) return head;
+    ListNode* dummy=new ListNode(-1);
+    ListNode* he=dummy;
+    dummy->next=head;
+    while(he->next!=NULL && he->next->next!=NULL){
+        ListNode*first=he->next;
+        ListNode*second=he->next->next;
+        first->next=second->next;
+        second->next=first;
+        he->next=second;
+        he=first;
+    }
+    return dummy->next;
     }
 };
+//          cur/first   sec  
+// 1 2 3 4=> 1           2    3
