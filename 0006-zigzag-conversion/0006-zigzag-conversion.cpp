f7af38ae3f3ccdd656189d9a19c30e96ab704aba
Yashaswini Rajanna <74413190+YashaswiniRajanna@users.noreply.github.com>
2025-02-20 17:41:42 +0530

    Time: 544 ms (5.02%), Space: 11.5 MB (75.99%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -4,21 +4,34 @@ public:
         if(numRows==1 || numRows> s.size()) return s;
         // vector<string> ans(numRows,"");
         string ans[numRows];
-
         for(auto i: ans){
             i="";
         }
-        int i=0;
+        int i=0,j=0; 
+        bool reach=true;
         while(i<s.size()){
-            for(int j=0;j<numRows && i<s.size();j++){
-                ans[j]+=s[i];
-                i++;
+            cout<<j<<i<<" "<<endl;
+            if(j == numRows-1 || j==0){
+                reach=(!reach);
             }
-            for(int k=numRows-2;k>0 && i<s.size();k--){
-                ans[k]+=s[i];
-                i++;
-            }
+            ans[j]+=s[i++];
+            if(reach==false){
+                j++;
+            } else{
+                j--;
+            }  
         }
+        //1st way 
+        // while(i<s.size()){
+        //     for(int j=0;j<numRows && i<s.size();j++){
+        //         ans[j]+=s[i];
+        //         i++;
+        //     }
+        //     for(int k=numRows-2;k>0 && i<s.size();k--){
+        //         ans[k]+=s[i];
+        //         i++;
+        //     }
+        // }
         for(auto i: ans){
             cout<<i<<" "<<endl;
         }
