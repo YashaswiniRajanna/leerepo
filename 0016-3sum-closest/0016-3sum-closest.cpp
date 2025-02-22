ae3129756eb6323f9cf0bb1789e3ef1f2f508e84
Yashaswini Rajanna <74413190+YashaswiniRajanna@users.noreply.github.com>
2025-02-22 17:53:49 +0530

    Time: 11 ms (63.37%), Space: 14 MB (21.64%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -1,20 +1,43 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
+        //bruteforce
+        // sort(nums.begin(),nums.end());
+        // if(nums.size()<3) return -1;
+        // int ans=(nums[0]+nums[1]+nums[2]);
+        // for(int i=0;i<nums.size()-2;i++){
+        //     for(int j=i+1;j<nums.size()-1;j++){
+        //         for(int k=j+1;k<nums.size();k++){
+        //             if(abs(target-ans) > abs(target-(nums[i]+nums[j]+nums[k]))){
+        //                 ans=(nums[i]+nums[j]+nums[k]);
+        //                 cout<<ans<<endl;
+        //             }
+        //         }
+        //     }
+        // }
+        // return ans;
+
+        //2pointer
+        if(nums.size()<3) return -1;
+        int ans=nums[0]+nums[1]+nums[2];
         sort(nums.begin(),nums.end());
-        if(nums.size()<2) return -1;
-        int ans=(nums[0]+nums[1]+nums[2]);
         for(int i=0;i<nums.size()-2;i++){
-            for(int j=i+1;j<nums.size()-1;j++){
-                for(int k=j+1;k<nums.size();k++){
-                    if(abs(target-ans) > abs(target-(nums[i]+nums[j]+nums[k]))){
-                        ans=(nums[i]+nums[j]+nums[k]);
-                        cout<<ans<<endl;
-                    }
-                    // ans=min(ans,(nums[i]+nums[j]+nums[k]));
+            int left=i+1,right=nums.size()-1;
+            while(left<right){
+                int sum=nums[i]+nums[left]+nums[right];
+                if(abs(target-ans)>abs(target-sum)){
+                    ans=sum;
                 }
+                if(target > sum){
+                    left++;
+                }
+                else if(target<sum){
+                    right--;
+                }
+                else
+                   return sum;   
             }
         }
-        return ans;
+            return ans;
     }
 };
