4228d7d781b33a5fd57b844a0de584420f23edcb
Yashaswini Rajanna <74413190+YashaswiniRajanna@users.noreply.github.com>
2025-02-23 13:38:48 +0530

    Time: 11 ms (13.08%), Space: 12.5 MB (43.28%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        map<int,string,greater<int>> mapp={ {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+        // map<int,string,greater<int>> mapp={ {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+        //     {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
+        //     {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"},
+        //     {1, "I"}};
+        // string res="";
+        // for(auto &i: mapp){
+        //     if(num==0) break;
+        //     while(num >= i.first){
+        //         res += i.second;
+        //         num -= i.first;
+        //     }
+        // }
+        // return res;
+
+      vector<pair<int, string>> roman = {
+            {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
             {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
             {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"},
-            {1, "I"}};
-        string res="";
-        for(auto &i: mapp){
-            if(num==0) break;
-            while(num >= i.first){
-                res += i.second;
-                num -= i.first;
+            {1, "I"}
+        };
+
+        string result = "";
+        for (auto &[value, symbol] : roman) {
+            while (num >= value) {
+                result += symbol;
+                num -= value;
             }
         }
-        return res;
-        
-    //   vector<pair<int, string>> roman = {
-    //         {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
-    //         {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
-    //         {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"},
-    //         {1, "I"}
-    //     };
-
-    //     string result = "";
-    //     for (auto &[value, symbol] : roman) {
-    //         while (num >= value) {
-    //             result += symbol;
-    //             num -= value;
-    //         }
-    //     }
-    //     return result;
+        return result;
 
     }
 };
