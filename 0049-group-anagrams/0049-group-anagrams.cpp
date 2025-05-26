86cdbc773b0a2e9401357440ea3758ba6a9772cb
Yashaswini Rajanna <74413190+YashaswiniRajanna@users.noreply.github.com>
2025-05-26 20:12:42 +0530

    Time: 24 ms (29.26%), Space: 25.9 MB (56.79%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,33 +1,14 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        //O(N* k logk)=>n-no of elements in strs, k -> maximum string hile sorting
-        // unordered_map<string,vector<string>> m;
-        // vector<vector<string>> res;
-        // for(auto s:strs){
-        //     string unsorted_str=s;
-        //     sort(s.begin(),s.end());
-        //     m[s].push_back(unsorted_str);
-        // }
-        // for(auto i:m){
-        //     res.push_back(i.second);
-        // }
-        // return res;
-        
-        vector<vector<string>>res;
-        unordered_map <string,vector<string>> m;
-        for(auto i: strs){
-            string r="";
-            int  c[26]={0};
-            for(auto j :i){
-                c[j-'a']++;
-            }
-            for(int j=0;j<26;j++){
-                r+= "#"+ to_string(c[j]);
-            }
-            m[r].push_back(i);
+        vector<vector<string>> res;
+        unordered_map<string,vector<string>>mp;
+        for (int i=0;i<strs.size();i++){
+            string temp= strs[i];
+            sort(strs[i].begin(),strs[i].end());
+            mp[strs[i]].push_back(temp);
         }
-        for(auto i: m){
+        for(auto i: mp){
             res.push_back(i.second);
         }
         return res;
