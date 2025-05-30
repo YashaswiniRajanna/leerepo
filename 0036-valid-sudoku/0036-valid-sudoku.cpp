bfe27a71cf1d89717dcf5a5a16bdebcf817b17d4
Yashaswini Rajanna <74413190+YashaswiniRajanna@users.noreply.github.com>
2025-05-30 12:05:12 +0530

    Time: 4 ms (54.31%), Space: 26 MB (7.98%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -3,30 +3,26 @@ public:
     bool isValidSudoku(vector<vector<char>>& board) {
         unordered_set<char> rows[9];
         unordered_set<char> cols[9];
-        unordered_set<char> boxes[9];
+        unordered_set<char> box[9];
         for(int i=0;i<board.size();i++){
             for(int j=0;j<board[0].size();j++){
-                char elem=board[i][j];
-                if(elem == '.'){
-                    continue;
+                if(board[i][j]!= '.'){
+                    if(rows[i].find(board[i][j])!=rows[i].end()){
+                        return false;
+                    }
+                    rows[i].insert(board[i][j]);
+                    if(cols[j].find(board[i][j])!=cols[j].end()){
+                        return false;
+                    }
+                    cols[j].insert(board[i][j]);
+                    int index=(i/3)*3+(j/3);
+                    if(box[index].find(board[i][j])!=box[index].end()){
+                        return false;
+                    }
+                    box[index].insert(board[i][j]);
                 }
-                if(rows[i].find(elem)!=rows[i].end()){
-                    return false;
-                }
-                rows[i].insert(elem);
-                
-                if(cols[j].find(elem)!=cols[j].end()){
-                    return false;
-                }
-                cols[j].insert(elem);
-                
-                int ind=(i/3)*3+(j/3);
-                if(boxes[ind].find(elem)!=rows[ind].end()){
-                    return false;
-                }
-                boxes[ind].insert(elem);
             }
         }
-        return true;
+    return true;
     }
 };
