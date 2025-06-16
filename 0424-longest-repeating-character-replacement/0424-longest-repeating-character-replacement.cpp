feb720fd305f9773743f7d163ab60ea5dad49c41
Yashaswini Rajanna <74413190+YashaswiniRajanna@users.noreply.github.com>
2025-06-16 22:18:51 +0530

    Time: 3 ms (73.43%), Space: 10.9 MB (41.94%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        unordered_map<char,int>m;
+        // unordered_map<char,int>m;
+        vector<int>m (26,0);
         int res=0,maxc=0,start=0,end=0;
         while(end<s.size()){
-            m[s[end]]++;
-            maxc=max(maxc,m[s[end]]);
+            m[s[end]-'A']++;
+            maxc=max(maxc,m[s[end]-'A']);
             while((end-start+1)-maxc>k){
-                m[s[start]]--;
+                m[s[start]-'A']--;
                 start++;
             }
             res=max(res,end-start+1);
