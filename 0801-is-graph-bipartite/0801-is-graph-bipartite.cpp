961b5da21011c686e1f0b48f92c468b118c888ec
Yashaswini Rajanna <74413190+YashaswiniRajanna@users.noreply.github.com>
2025-07-31 10:03:19 +0530

    Time: 0 ms (100%), Space: 18.8 MB (8.01%) - LeetHub

---
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isBipartite(vector<vector<int>>& grid) {
+        int n=grid.size();
+        vector<int>colors(n,-1);
+        bool colo=0;
+        cout<<colo;
+        for(int i=0;i<n;i++){
+            queue<pair<int,bool>>q;
+            if(colors[i]==-1){
+                 q.push({i,!colo});
+                 while(!q.empty()){
+                    int val=q.front().first;
+                    bool col=q.front().second;
+                    q.pop();
+                    for(auto i: grid[val]){
+                        if(colors[i]==-1){
+                             colors[i]=!col;
+                             q.push({i,colors[i]});
+                        }
+                        else if(colors[i]==col){
+                            return false;
+                        }
+                    }
+
+                }
+
+            }
+        }
+        return true;
+    }
+};
