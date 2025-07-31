d278e98e0f6b28177d53dc107be890e764f5a0ef
Yashaswini Rajanna <74413190+YashaswiniRajanna@users.noreply.github.com>
2025-07-31 23:14:05 +0530

    Time: 97 ms (71.43%), Space: 62.1 MB (96.62%) - LeetHub

--- 1780-lowest-common-ancestor-of-a-binary-tree-ii/1780-lowest-common-ancestor-of-a-binary-tree-ii.cpp
+++ 1780-lowest-common-ancestor-of-a-binary-tree-ii/1780-lowest-common-ancestor-of-a-binary-tree-ii.cpp
@@ -1,46 +1,82 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     bool p_flag=false,q_flag=false;
+//     TreeNode* helper(TreeNode* root, TreeNode* p, TreeNode* q){
+//         if(root==NULL){
+//             return NULL;
+//         }
+//         TreeNode* left=helper(root->left,p,q);
+//         TreeNode* right=helper(root->right,p,q);
+//         if(root==p){
+//             p_flag=true;
+//             return root;
+//         }
+//         else if(q==root){
+//             q_flag=true;
+//             return root;
+//         }
+
+//         if(left && right){
+//             return root;
+//         }
+//         // return left==NULL ? right :left;
+//         else if(!left){
+//             return right;
+//         }
+//         else{
+//             return left;
+//         }
+//     }
+//     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+//         TreeNode* ret=helper(root,p,q);
+//         if(!p_flag || !q_flag){
+//             return NULL;
+//         }
+//         return ret;
+//     }
+// };
 class Solution {
 public:
-    bool p_flag=false,q_flag=false;
-    TreeNode* helper(TreeNode* root, TreeNode* p, TreeNode* q){
-        if(root==NULL){
-            return NULL;
+    void search(TreeNode* root, TreeNode * dest, bool &flag){
+        if(!root) return;
+        if(root == dest){
+            flag = true;
+            return;
         }
-        TreeNode* left=helper(root->left,p,q);
-        TreeNode* right=helper(root->right,p,q);
-        if(root==p){
-            p_flag=true;
-            return root;
-        }
-        else if(q==root){
-            q_flag=true;
-            return root;
-        }
+        search(root->left,dest,flag);
+        search(root->right,dest,flag);
 
-        if(left && right){
-            return root;
-        }
-        // return left==NULL ? right :left;
-        else if(!left){
-            return right;
-        }
-        else{
-            return left;
-        }
+        return;
+    }
+public:
+    TreeNode* lca(TreeNode* root,  TreeNode* p, TreeNode* q){
+        if(!root) return NULL;
+
+        if(root == p || root == q) return root;
+
+        TreeNode* left = lca(root->left,p,q);
+        TreeNode* right = lca(root->right,p,q);
+
+        if(left && right) return root;
+        if(!left) return right;
+        return left;
     }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        TreeNode* ret=helper(root,p,q);
-        if(!p_flag || !q_flag){
-            return NULL;
-        }
-        return ret;
+        if(!root) return NULL;
+        bool f1=false,f2=false;
+        search(root,p,f1);
+        search(root,q,f2);
+        if(!f1 || !f2) return NULL;
+
+        return lca(root,p,q);
     }
 };
