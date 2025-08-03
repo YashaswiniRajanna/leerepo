86992261559591c420edc8c77958eb37c821fae0
Yashaswini Rajanna <74413190+YashaswiniRajanna@users.noreply.github.com>
2025-08-03 12:58:10 +0530

    Time: 60 ms (6.43%), Space: 28.8 MB (5.21%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> ans;
+        vector<int> tstart;
+        vector<int>tend;
+
+        if(intervals.size()==1){
+            return intervals;
+        }
+        sort(intervals.begin(),intervals.end());
+        ans.push_back({intervals[0][0],intervals[0][1]});
+        for(int i=1;i<intervals.size();i++){
+            if(ans[ans.size()-1][1]>=intervals[i][0])
+        {
+            vector<int> temp;
+            if(intervals[i][1]>=ans[ans.size()-1][1]){
+                temp={ans[ans.size()-1][0],intervals[i][1]};
+            }
+            else{
+                temp={ans[ans.size()-1][0],ans[ans.size()-1][1]};
+            }
+            ans.pop_back();
+            ans.push_back(temp);
+        }
+        else{
+            ans.push_back({intervals[i][0],intervals[i][1]});
+        }
+        }
+        for(auto i: ans){
+            cout<<i[0]<<" "<<i[1]<<endl;
+        }
+        set<vector<int>> s;
+        for(auto i: ans){
+            s.insert(i);
+        }
+        vector<vector<int>> final_ans(s.begin(),s.end());
+        return final_ans;
+        // int i=0,j=0;
+        // while(i<tstart.size()){
+        //     if(tstart[i]>tend[j]){
+
+        //     }
+        // }
+
+
+    }
+};
