0b4d64ba913b0085bc33b8a8b8c44cffec5010d1
Yashaswini Rajanna <74413190+YashaswiniRajanna@users.noreply.github.com>
2025-08-07 09:44:16 +0530

    Time: 30 ms (8.91%), Space: 26.3 MB (64.11%) - LeetHub

---
+++ 0348-design-tic-tac-toe/0348-design-tic-tac-toe.cpp
@@ -0,0 +1,55 @@
+class TicTacToe {
+private: 
+    int dim;
+    vector<vector<char>> board;
+public:
+    TicTacToe(int n) {
+        this->dim=n;
+        int i=0;
+        vector<char> temp(n,'-1');
+        while(i<n){
+            board.push_back(temp);
+            i++;
+        }
+    }
+    
+    int move(int row, int col, int player) {
+        //  1->X  2->O
+        char val=(player==1) ? 'X':'O';
+        this->board[row][col]= val;
+        int size=this->dim;
+        bool horizontal=true,vertical=true,diagonal_left=true,diagonal_right=true;
+        for(int i=0;i<size;i++){
+           if(this->board[i][col]!=val){
+                vertical=false;
+            } 
+        }
+        for(int j=0;j<size;j++){
+           if(this->board[row][j]!=val){
+                horizontal=false;
+            } 
+        }
+        for(int i=0;i<size;i++){
+               if(this->board[i][i]!=val){
+                    diagonal_left=false;
+                } 
+            }
+        for(int i=0;i<size;i++){
+               if(this->board[i][size-1-i]!=val){
+                    diagonal_right=false;
+                } 
+            }
+        if(horizontal || vertical || diagonal_left || diagonal_right){
+                return player;
+            }
+        else{
+            return 0;
+        }
+    }
+};
+
+/**
+ * Your TicTacToe object will be instantiated and called as such:
+ * TicTacToe* obj = new TicTacToe(n);
+ * int param_1 = obj->move(row,col,player);
+ */
