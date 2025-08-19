7f263851a735473df69d28d4f03f5651708b8ea3
Yashaswini Rajanna <74413190+YashaswiniRajanna@users.noreply.github.com>
2025-08-19 22:11:36 +0530

    Time: 4 ms (68.49%), Space: 19.8 MB (51.47%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,62 +1,69 @@
 class Solution {
 public:
-    // void dfs(int &node,vector<vector<int>>& adj,stack<int>& st,vector<int>&visit){
-    //     for(auto i: adj[node]){
-    //         if(!visit[i]){
-    //             visit[i]=i;
-    //             dfs(i,adj,st,visit);
-    //         }
-    //     }
-    //     st.push(node);
-    // }
+    bool dfs(int &node,vector<vector<int>>& adj,stack<int>& st,vector<int>&visit,vector<int>&pathvisit){
+        visit[node]=1;
+        pathvisit[node]=1;
+        for(auto i: adj[node]){
+            if(!visit[i]){
+                if(!dfs(i,adj,st,visit,pathvisit)) return false;
+            }
+            else if(pathvisit[i]){
+                return false;
+            }
+        }
+        st.push(node);
+        pathvisit[node]=0;
+        return true;
+    }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        queue<int>q;
-        vector<int> toporder;
-        vector<int>ingress(numCourses,0);
+        stack<int> st;
+        vector<int> topo;
+        vector<int> visit(numCourses,0);
+        vector<int> pathvisit(numCourses,0);
         vector<vector<int>>adj(numCourses,vector<int>{});
         for(int i=0;i<prerequisites.size();i++){
             adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
         }
-        for(int i=0;i<prerequisites.size();i++){
-            cout<<endl;
-            cout<<prerequisites[i][0]<<prerequisites[i][1]<<endl;
-            ingress[prerequisites[i][0]]++;
-        }
-        for(int i=0;i<ingress.size();i++){
-            if(ingress[i]==0){
-                q.push(i);
+        for(int i=0;i<numCourses;i++){
+            if(!visit[i]){
+                if(!dfs(i,adj,st,visit,pathvisit)){
+                    return false;
+                }
             }
         }
-        while(!q.empty()){
-            int node =q.front();
-            q.pop();
-            toporder.push_back(node);
-            for(auto i: adj[node]){
-                ingress[i]--;
-                if(ingress[i]==0) q.push(i);
-            }
-        }
-        if(toporder.size() < numCourses){
+    
+        if(numCourses < st.size()){
             return false;
         }
-        cout<<toporder.size();
         return true;
-        // stack<int> st;
-        // vector<int> visit(numCourses,0);
+        // queue<int>q;
+        // vector<int> toporder;
+        // vector<int>ingress(numCourses,0);
         // vector<vector<int>>adj(numCourses,vector<int>{});
         // for(int i=0;i<prerequisites.size();i++){
-        //     adj[prerequisites[i][0]].push_back(prerequisites[i][1]);
+        //     adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
         // }
-        // for(int i=0;i<numCourses;i++){
-        //     if(!visit[i]){
-        //         visit[i]=1;
-        //         dfs(i,adj,st,visit);
+        // for(int i=0;i<prerequisites.size();i++){
+        //     ingress[prerequisites[i][0]]++;
+        // }
+        // for(int i=0;i<ingress.size();i++){
+        //     if(ingress[i]==0){
+        //         q.push(i);
         //     }
         // }
-        // cout<<st.size();
-        // if(numCourses > st.size()){
+        // while(!q.empty()){
+        //     int node =q.front();
+        //     q.pop();
+        //     toporder.push_back(node);
+        //     for(auto i: adj[node]){
+        //         ingress[i]--;
+        //         if(ingress[i]==0) q.push(i);
+        //     }
+        // }
+        // if(toporder.size() < numCourses){
         //     return false;
         // }
         // return true;
+ 
     }
 };
