68341cda7fd9cdeb454bc320e3746979746c99ba
Yashaswini Rajanna <74413190+YashaswiniRajanna@users.noreply.github.com>
2025-08-26 09:24:23 +0530

    Time: 49 ms (60.16%), Space: 13.6 MB (64.1%) - LeetHub

--- 0277-find-the-celebrity/0277-find-the-celebrity.cpp
+++ 0277-find-the-celebrity/0277-find-the-celebrity.cpp
@@ -3,26 +3,34 @@
 
 class Solution {
 public:
-    set<int> s;
+    // set<int> s;
     bool iscelebrity(int i,int n){
         for(int j=0;j<n;j++){
             if(i==j) continue;
             if(knows(i,j) || !knows(j,i)){
-                s.erase(i);
+                // s.erase(i);
                 return false;
             }
-            else if(knows(j,i)){
-                s.erase(j);
-            }
+            // else if(knows(j,i)){
+            //     s.erase(j);
+            // }
         }
         return true;
     }
     int findCelebrity(int n) {
-        
+        int celebrity=0;
         for(int i=0;i<n;i++){
-            s.insert(i);
-            if(iscelebrity(i,n) && s.count(i)>0){
-                return i;
+            if(knows(celebrity,i)){
+                celebrity=i;
+            }
+            // s.insert(i);
+            // if(iscelebrity(i,n) && s.count(i)>0){
+            //     return i;
+            // }
+        }
+        for(int j=0;j<n;j++){
+            if(iscelebrity(celebrity,n)){
+                return celebrity;
             }
         }
         return -1;
